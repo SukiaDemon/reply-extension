e994273220debd0a47ca7907cf8eca743298377a
Arcsery <102926693+Arcsery@users.noreply.github.com>
2025-01-03 15:55:13 +0100

    Add files via upload

---
+++ BCA-1.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         BCA
+// @namespace    https://www.bondageprojects.com/
+// @version      1
+// @description  Bondage club additions.
+// @author       agicitag
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match http://localhost:*/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var script = document.createElement("script");
+    script.src = "https://arcsery.github.io/reply-extension//bca.js";
+    document.head.appendChild(script);
+})();
